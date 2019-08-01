76cafb9ff1b6508513e78f6d56ddaac93d40b0c6
Rod Johnson <rod@atomist.com>
2019-08-02 07:46:24 +1000

    Autofix: Spring format
    [atomist:generated] [atomist:autofix=spring_format]

--- src/main/java/r1/Toro1Controller.java
+++ src/main/java/r1/Toro1Controller.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package r1; 
+package r1;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
